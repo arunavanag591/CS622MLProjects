c11868f8f6fe89f2ab2880514e23a5f4fb7a5551
arunavanag591 <arunavoster@gmail.com>
2020-10-24 18:35:03 -0700

    Project 2 All Files

--- Project2/gradient_descent.py
+++ Project2/gradient_descent.py
@@ -1,9 +1,19 @@
+#Arunava Nag; CS 622 Project 2
 #!/usr/bin/env python
 import numpy as np
 import random
 import math as m
 
+#function to find gradient descent 
 
-def gradient_descent(df_test2,np.array([]),0.1):
-    return x    
-    
+def gradient_descent(f, x_init, eta):
+    precision = 0.00001                 #value the gradient will get close to
+    max_iter = 1000                     #limiting max epochs
+    count = 0                           #counter for the epochs to reach precision
+    step_size = [eta,eta]               #declaring a initial stepsize
+    while any(a > precision for a in step_size) and count < max_iter:           
+        x = x_init                      #storing old x
+        x_init = x_init - eta*f(x)      #gradient descent
+        step_size = abs(x_init - x)     
+        count+=1    
+    return x
