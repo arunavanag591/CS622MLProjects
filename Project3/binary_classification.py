957bee8d4218772526dafc8da36c6ff97fe11651
arunavanag591 <arunavoster@gmail.com>
2020-11-17 23:00:21 -0800

    binary classification

--- Project3/binary_classification.py
+++ Project3/binary_classification.py
@@ -5,17 +5,22 @@ import sys
 import matplotlib.pyplot as plt
 
 # def distance_point_to_hyperplane(pt, w, b):
-    
 # def compute_margin(data, w, b):
-
 # def svm_test_brute(w,b,x):
 
-def calculateWB(vectors):
-    w1 = vectors[:,0]
-    w2 = vectors[:,1]
-    y = vectors[:,2]
-   
-    
+
+
+def getWB(vectors):
+    rows,column = vectors.shape
+    arr = np.zeros(shape=(rows,2))
+    arr[:, 0], arr[:, 1] = vectors[:, 0], vectors[:, 1]
+    direction = arr[0]-arr[1]
+    gamma = (np.linalg.norm(arr[0] - arr[1]))/2
+    norm_dir=(np.sqrt(np.square(direction[0])+np.square(direction[1])))
+    w = (direction/norm_dir)*gamma
+    b = -1 - np.dot(w,arr[1])
+    return w,b
+
     
 def closestpoints(points):
     dist=[]
@@ -27,23 +32,17 @@ def closestpoints(points):
     idx = np.argpartition(dist, k)[:k]
     return idx
     
-# def find_largest_distance(point_pos, point_neg)
-    
-#     for i in range(len(point_pos)):
-#         for j in range(len(point_neg)):
-#             dist.append(np.linalg.norm(point_pos[i]-point_neg[j]))        
-
 def svm_train_brute(data):
     
     rows, columns = data.shape 
+    points = np.zeros(shape=(rows, 2))
     points_pos=np.zeros(shape=(5,2))
     points_neg=np.zeros(shape=(5,2))
     points_pos[:, 0], points_pos[:, 1] = data[0:5, 0], data[0:5, 1]
     points_neg[:, 0], points_neg[:, 1] = data[5:10, 0], data[5:10, 1]
     points[:, 0], points[:, 1] = data[:, 0], data[:, 1] #list containing only features
     dist = closestpoints(points)
+    
     vectors = data[dist,]
-   # w,b = calculateWB(vectors)
-    margin = np.linalg.norm(a - b)
-    gamma = margin/2
-    print margin   
+    w,b = getWB(vectors)
+    return w,b,vectors
---
+++ Project3/multiclass_classification.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+def svm_train_multiclass([data,Y]):
+    print data
